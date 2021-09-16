85fe4c9eee606b525de74bc49e53a3cffeb672e2
chenf <baiynui@hotmail.com>
2021-09-16 11:32:02 +0800

    Remove unsupported methods from UniswapV3PoolAdapter

--- contracts/UniswapV3PoolAdapter.sol
+++ contracts/UniswapV3PoolAdapter.sol
@@ -17,7 +17,7 @@ import "./uniswap/interfaces/callback/IUniswapV3SwapCallback.sol";
 import "hardhat/console.sol";
 
 /// @dev UniswapV3PoolAdapter
-contract UniswapV3PoolAdapter is CoFiXBase, ICoFiXPool, IUniswapV3SwapCallback {
+contract UniswapV3PoolAdapter is CoFiXBase,/* ICoFiXPool, */ IUniswapV3SwapCallback {
 
     /* ******************************************************************************************
      * Note: In order to unify the authorization entry, all transferFrom operations are carried
@@ -55,70 +55,70 @@ contract UniswapV3PoolAdapter is CoFiXBase, ICoFiXPool, IUniswapV3SwapCallback {
         WETH9 = weth9;
     }
 
-    /// @dev Set configuration
-    /// @param theta Trade fee rate, ten thousand points system. 20
-    /// @param impactCostVOL Impact cost threshold, this parameter is obsolete
-    /// @param nt Each unit token (in the case of binary pools, eth) is used for the standard ore output, 1e18 based
-    function setConfig(uint16 theta, uint96 impactCostVOL, uint96 nt) external override {
-        revert("UWP:not support");
-    }
+    // /// @dev Set configuration
+    // /// @param theta Trade fee rate, ten thousand points system. 20
+    // /// @param impactCostVOL Impact cost threshold, this parameter is obsolete
+    // /// @param nt Each unit token (in the case of binary pools, eth) is used for the standard ore output, 1e18 based
+    // function setConfig(uint16 theta, uint96 impactCostVOL, uint96 nt) external override {
+    //     revert("UWP:not support");
+    // }
 
-    /// @dev Get configuration
-    /// @return theta Trade fee rate, ten thousand points system. 20
-    /// @return impactCostVOL Impact cost threshold, this parameter is obsolete
-    /// @return nt Each unit token (in the case of binary pools, eth) is used for the standard ore output, 1e18 based
-    function getConfig() external view override returns (uint16 theta, uint96 impactCostVOL, uint96 nt) {
-        revert("UWP:not support");
-    }
+    // /// @dev Get configuration
+    // /// @return theta Trade fee rate, ten thousand points system. 20
+    // /// @return impactCostVOL Impact cost threshold, this parameter is obsolete
+    // /// @return nt Each unit token (in the case of binary pools, eth) is used for the standard ore output, 1e18 based
+    // function getConfig() external view override returns (uint16 theta, uint96 impactCostVOL, uint96 nt) {
+    //     revert("UWP:not support");
+    // }
 
-    /// @dev Add liquidity and mint xtoken
-    /// @param token Target token address
-    /// @param to The address to receive xtoken
-    /// @param amountETH The amount of ETH added to pool. (When pool is AnchorPool, amountETH is 0)
-    /// @param amountToken The amount of Token added to pool
-    /// @param payback As the charging fee may change, it is suggested that the caller pay more fees, 
-    /// and the excess fees will be returned through this address
-    /// @return xtoken The liquidity share token address obtained
-    /// @return liquidity The real liquidity or XToken minted from pool
-    function mint(
-        address token,
-        address to, 
-        uint amountETH, 
-        uint amountToken,
-        address payback
-    ) external payable override returns (
-        address xtoken,
-        uint liquidity
-    ) {
-        revert("UWP:not support");
-    }
+    // /// @dev Add liquidity and mint xtoken
+    // /// @param token Target token address
+    // /// @param to The address to receive xtoken
+    // /// @param amountETH The amount of ETH added to pool. (When pool is AnchorPool, amountETH is 0)
+    // /// @param amountToken The amount of Token added to pool
+    // /// @param payback As the charging fee may change, it is suggested that the caller pay more fees, 
+    // /// and the excess fees will be returned through this address
+    // /// @return xtoken The liquidity share token address obtained
+    // /// @return liquidity The real liquidity or XToken minted from pool
+    // function mint(
+    //     address token,
+    //     address to, 
+    //     uint amountETH, 
+    //     uint amountToken,
+    //     address payback
+    // ) external payable override returns (
+    //     address xtoken,
+    //     uint liquidity
+    // ) {
+    //     revert("UWP:not support");
+    // }
 
-    /// @dev Maker remove liquidity from pool to get ERC20 Token and ETH back (maker burn XToken) 
-    /// @param token The address of ERC20 Token
-    /// @param to The target address receiving the Token
-    /// @param liquidity The amount of liquidity (XToken) sent to pool, or the liquidity to remove
-    /// @param payback As the charging fee may change, it is suggested that the caller pay more fees, 
-    /// and the excess fees will be returned through this address
-    /// @return amountETHOut The real amount of ETH transferred from the pool
-    /// @return amountTokenOut The real amount of Token transferred from the pool
-    function burn(
-        address token,
-        address to, 
-        uint liquidity, 
-        address payback
-    ) external payable override returns (
-        uint amountETHOut,
-        uint amountTokenOut 
-    ) {
-        revert("UWP:not support");
-    }
+    // /// @dev Maker remove liquidity from pool to get ERC20 Token and ETH back (maker burn XToken) 
+    // /// @param token The address of ERC20 Token
+    // /// @param to The target address receiving the Token
+    // /// @param liquidity The amount of liquidity (XToken) sent to pool, or the liquidity to remove
+    // /// @param payback As the charging fee may change, it is suggested that the caller pay more fees, 
+    // /// and the excess fees will be returned through this address
+    // /// @return amountETHOut The real amount of ETH transferred from the pool
+    // /// @return amountTokenOut The real amount of Token transferred from the pool
+    // function burn(
+    //     address token,
+    //     address to, 
+    //     uint liquidity, 
+    //     address payback
+    // ) external payable override returns (
+    //     uint amountETHOut,
+    //     uint amountTokenOut 
+    // ) {
+    //     revert("UWP:not support");
+    // }
 
-    /// @dev Gets the token address of the share obtained by the specified token market making
-    /// @param token Target token address
-    /// @return If the fund pool supports the specified token, return the token address of the market share
-    function getXToken(address token) external view override returns (address) {
-        revert("UWP:not support");
-    }
+    // /// @dev Gets the token address of the share obtained by the specified token market making
+    // /// @param token Target token address
+    // /// @return If the fund pool supports the specified token, return the token address of the market share
+    // function getXToken(address token) external view override returns (address) {
+    //     revert("UWP:not support");
+    // }
 
     /// @dev Swap token
     /// @param src Src token address
@@ -135,11 +135,14 @@ contract UniswapV3PoolAdapter is CoFiXBase, ICoFiXPool, IUniswapV3SwapCallback {
         uint amountIn, 
         address to, 
         address payback
-    ) external payable override returns (
+    ) external payable returns (
         uint amountOut, 
         uint mined
     ) {
-        require(amountIn < 0x8000000000000000000000000000000000000000000000000000000000000000, "UWP:amountIn too large");
+        require(
+            amountIn < 0x8000000000000000000000000000000000000000000000000000000000000000, 
+            "UWP:amountIn too large"
+        );
 
         // 1. Return unnecessary eth
         // The src is 0, which means that the ETH is transferred in and the part exceeding
@@ -166,24 +169,6 @@ contract UniswapV3PoolAdapter is CoFiXBase, ICoFiXPool, IUniswapV3SwapCallback {
         }
 
         // 3. 调用uniswap v3 pool执行交易
-        // /// @notice Swap token0 for token1, or token1 for token0
-        // /// @dev The caller of this method receives a callback in the form of IUniswapV3SwapCallback#uniswapV3SwapCallback
-        // /// @param recipient The address to receive the output of the swap
-        // /// @param zeroForOne The direction of the swap, true for token0 to token1, false for token1 to token0
-        // /// @param amountSpecified The amount of the swap, which implicitly configures the swap as exact input (positive), or exact output (negative)
-        // /// @param sqrtPriceLimitX96 The Q64.96 sqrt price limit. If zero for one, the price cannot be less than this
-        // /// value after the swap. If one for zero, the price cannot be greater than this value after the swap
-        // /// @param data Any data to be passed through to the callback
-        // /// @return amount0 The delta of the balance of token0 of the pool, exact when negative, minimum when positive
-        // /// @return amount1 The delta of the balance of token1 of the pool, exact when negative, minimum when positive
-        // function swap(
-        //     address recipient,
-        //     bool zeroForOne,
-        //     int256 amountSpecified,
-        //     uint160 sqrtPriceLimitX96,
-        //     bytes calldata data
-        // ) external returns (int256 amount0, int256 amount1);
-
         (int256 amount0, int256 amount1) = IUniswapV3Pool(TARGET_UNISWAP_V3_POOL).swap(
             address(this),
             zeroForOne,
