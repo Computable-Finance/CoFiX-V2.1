1b35c40351c8e0912903d381c44232fcd233cd59
chenf <baiynui@hotmail.com>
2022-04-23 11:05:40 +0800

    Deploy to bsc main

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,6 @@
 //
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const deploy = require('./simple.proxy.js');
+const deploy = require('./bsc_main@20220423.js');
 
 exports.deploy = deploy.deploy;
